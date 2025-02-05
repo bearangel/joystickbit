82aaea60b744a84cfead02cb633e0d09629b8e35
bearangel <stbearangel@gmail.com>
2025-02-05 15:15:42 +0800

    优化遥控器

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
+def isJoystickTrigger(joystickValue: number):
+    if joystickValue > 130 or joystickValue < 126:
+        return True
+    return False
+def encode_frame(gamepad_id: number, joystick1_x: number, joystick1_y: number, joystick2_x: number, joystick2_y: number):
+    bytearray
+rigth_y = 0
+rigth_x = 0
+left_y = 0
+left_x = 0
+frame_format = "BhhhhH"
+radio.set_group(10)
+
 def on_forever():
-    pass
+    global left_x, left_y, rigth_x, rigth_y
+    left_x = joystick.Gamepad_Wiggly(Wiggly.JOYSTICK_LEFT_WI, Shaft.JOYSTICK_X_SHAFT)
+    left_y = joystick.Gamepad_Wiggly(Wiggly.JOYSTICK_LEFT_WI, Shaft.JOYSTICK_Y_SHAFT)
+    rigth_x = joystick.Gamepad_Wiggly(Wiggly.JOYSTICK_RIGHT_WI, Shaft.JOYSTICK_X_SHAFT)
+    rigth_y = joystick.Gamepad_Wiggly(Wiggly.JOYSTICK_RIGHT_WI, Shaft.JOYSTICK_Y_SHAFT)
+    radio.send_value("left_x", left_x)
+    radio.send_value("left_y", left_y)
+    radio.send_value("rigth_x", rigth_x)
+    radio.send_value("rigth_y", rigth_y)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,7 +1,44 @@
-radio.setGroup(10)
+function setJoystickRange (srcRange: number) {
+    newRange = Math.constrain(srcRange, minRange, maxRange)
+    if (Math.abs(newRange) < 10) {
+        // 添加死区
+        newRange = 0
+    }
+    return newRange
+}
+let vr = 0
+let vy = 0
+let vx = 0
+let newRange = 0
+let minRange = 0
+let maxRange = 0
+let rr = 0
+let lr = 0
+let rf = 0
+let lf = 0
+basic.showIcon(IconNames.House)
+maxRange = 255
+minRange = -255
+radio.setGroup(1)
+radio.setTransmitPower(7)
 basic.forever(function () {
-    radio.sendValue("Left_x", joystick.Gamepad_Wiggly(Wiggly.JOYSTICK_left_wi, Shaft.JOYSTICK_X_Shaft))
-    radio.sendValue("Left_y", joystick.Gamepad_Wiggly(Wiggly.JOYSTICK_left_wi, Shaft.JOYSTICK_Y_Shaft))
-    radio.sendValue("Rigth_x", joystick.Gamepad_Wiggly(Wiggly.JOYSTICK_right_wi, Shaft.JOYSTICK_X_Shaft))
-    radio.sendValue("Right_y", joystick.Gamepad_Wiggly(Wiggly.JOYSTICK_right_wi, Shaft.JOYSTICK_Y_Shaft))
+    // 设置左x轴范围,前进后退
+    vx = Math.map(joystick.Gamepad_Wiggly(Wiggly.JOYSTICK_left_wi, Shaft.JOYSTICK_X_Shaft), 0, 255, minRange, maxRange)
+    vx = setJoystickRange(vx)
+    // 设置左Y轴范围，左移右移
+    vy = Math.map(joystick.Gamepad_Wiggly(Wiggly.JOYSTICK_left_wi, Shaft.JOYSTICK_Y_Shaft), 0, 255, minRange, maxRange)
+    vy = setJoystickRange(vy)
+    // 设置右X轴范围，左右旋转
+    vr = Math.map(joystick.Gamepad_Wiggly(Wiggly.JOYSTICK_right_wi, Shaft.JOYSTICK_X_Shaft), 0, 255, minRange, maxRange)
+    vr = setJoystickRange(vr)
+    // 计算4个轮子的速度, 并作限幅处理
+    // 左前轮
+    lf = Math.constrain(vy + vx + vr, minRange, maxRange)
+    // 右前轮
+    rf = Math.constrain(vy - vx - vr, minRange, maxRange)
+    // 左后轮
+    lr = Math.constrain(vy - vx + vr, minRange, maxRange)
+    // 右后轮
+    rr = Math.constrain(vy + vx - vr, minRange, maxRange)
+    radio.sendString("" + (`${lf},${rf},${lr},${rr}`))
 })
